251da9ed2a77540a9e722bf3855917e2bb429705
Prakhar Sharma <prakhar.sharma_cs20@gla.ac.in>
2023-06-22 03:35:21 +0530

    Adding new files

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,5 @@
 const express=require('express');
-const { getAllProducts,createProduct,updateProduct, deleteProduct,getProductDetails, createProductReview  } = require('../controllers/productController');
+const { getAllProducts,createProduct,updateProduct, deleteProduct,getProductDetails, createProductReview, getProductReviews, deleteReview  } = require('../controllers/productController');
 const { isAuthenticatedUser,authorizeRoles } = require('../middleware/auth');
 
 
@@ -9,6 +9,7 @@ router.route('/products').get( getAllProducts);
 router.route('/product/new').post(isAuthenticatedUser,authorizeRoles("admin"),createProduct);
 router.route('/product/:id').put(isAuthenticatedUser,authorizeRoles("admin"),updateProduct).delete(isAuthenticatedUser,authorizeRoles("admin"),deleteProduct).get(getProductDetails);
 router.route("/review").put(isAuthenticatedUser,createProductReview);
+router.route("/reviews").get(getProductReviews).delete(isAuthenticatedUser,deleteReview);
 
 
 module.exports=router
